13feb749c017ddccd9341dfafaa536396f59fc70
Claudio Pi <claudiopi@gmail.com>
2024-11-26 16:08:11 -0300

    Inventory Management initial version

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { IGX_LIST_DIRECTIVES, IgxAvatarComponent, IgxIconComponent } from 'igniteui-angular';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet, IGX_LIST_DIRECTIVES, IgxAvatarComponent, IgxIconComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
